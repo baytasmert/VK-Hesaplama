b057e990ea8b07cca17f03f4174548d809d21b3a
Mert Bayats <mertbaytas@gmail.com>
2023-02-23 15:09:02 +0300

    selam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double boy,kilo,vki;
+        Scanner oku=new Scanner(System.in);
+
+        System.out.print("Boyunuzu Giriniz (metre): ");
+        boy=oku.nextDouble();
+
+        System.out.print("\nKilonuzu Giriniz (kg): ");
+        kilo=oku.nextDouble();
+
+        vki=kilo/Math.pow(boy,2);
+        System.out.println("VKİ: "+vki);
+
+
     }
 }
